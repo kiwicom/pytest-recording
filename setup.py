71b241fe94aced55ef3259b6c8932562b139ce68
dmitry.dygalo <dmitry.dygalo@kiwi.com>
2019-07-18 16:28:41 +0200

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(fname):
 
 setup(
     name="pytest-recording",
-    version="0.1.0",
+    version="0.2.0",
     author="Stanislav Tkachenko",
     author_email="stanislav.tkachenko@kiwi.com",
     maintainer="Stanislav Tkachenko",
