13b36bc4b4c5f8c2e27759f93cb1dbfe4461654c
d.dygalo <dadygalo@gmail.com>
2019-08-01 08:13:12 +0200

    Add missing `long_description_content_type` in `setup.py`

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
     url="https://github.com/kiwicom/pytest-recording",
     description="A pytest plugin that allows to record network interactions via VCR.py",
     long_description=read("README.rst"),
+    long_description_content_type="text/x-rst",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
