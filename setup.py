ed443042901a923ebb83652823f0ed52e350d49e
Dmitry Dygalo <dadygalo@gmail.com>
2019-08-18 14:15:55 +0200

    Release 0.3.3

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(fname):
 
 setup(
     name="pytest-recording",
-    version="0.3.2",
+    version="0.3.3",
     author="Dmitry Dygalo",
     author_email="dmitry.dygalo@kiwi.com",
     maintainer="Dmitry Dygalo",
